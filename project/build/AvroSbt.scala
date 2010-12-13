043139bf305a18a91a63e2cc5e5685f74d382b5f
Coda Hale <coda.hale@gmail.com>
2010-12-13 10:48:31 -0800

    Move to sbt 0.7.5RC0, sbt-idea, and Ivy.

--- project/build/AvroSbt.scala
+++ project/build/AvroSbt.scala
@@ -1,16 +1,15 @@
-class AvroSbt(info: sbt.ProjectInfo) extends sbt.PluginProject(info) {
+import sbt._
+
+class AvroSbt(info: ProjectInfo) extends PluginProject(info) with IdeaProject {
   val avro = "org.apache.hadoop" % "avro" % "1.3.2" withSources()
   val jopt = "net.sf.jopt-simple" % "jopt-simple" % "3.2" withSources()
   
+
   /**
-   * Publish to a local temp repo, then rsync the files over to repo.codahale.com.
+   * Publish via Ivy.
    */
-  override def managedStyle = sbt.ManagedStyle.Maven
-  val publishTo = sbt.Resolver.file("Local Cache", ("." / "target" / "repo").asFile)
-  def publishToLocalRepoAction = super.publishAction
-  override def publishAction = task {
-    log.info("Uploading to repo.codahale.com")
-    sbt.Process("rsync", "-avz" :: "target/repo/" :: "codahale.com:/home/codahale/repo.codahale.com" :: Nil) ! log
-    None
-  } describedAs("Publish binary and source JARs to repo.codahale.com") dependsOn(test, publishToLocalRepoAction)
+  lazy val publishTo = Resolver.sftp("Personal Repo",
+                                     "codahale.com",
+                                     "/home/codahale/repo.codahale.com/") as ("codahale")
+  override def managedStyle = ManagedStyle.Maven
 }
