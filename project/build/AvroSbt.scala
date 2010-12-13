28878466c3aee4de5a5419a631ed81d0bfd93442
Coda Hale <coda.hale@gmail.com>
2010-12-13 10:57:06 -0800

    Upgrade to Avro 1.4.1.

--- project/build/AvroSbt.scala
+++ project/build/AvroSbt.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 class AvroSbt(info: ProjectInfo) extends PluginProject(info) with IdeaProject {
-  val avro = "org.apache.hadoop" % "avro" % "1.3.2" withSources()
-  val jopt = "net.sf.jopt-simple" % "jopt-simple" % "3.2" withSources()
+  val avro = "org.apache.avro" % "avro" % "1.4.1" withSources() intransitive()
+  val jopt = "net.sf.jopt-simple" % "jopt-simple" % "3.2" withSources() intransitive()
   
 
   /**
