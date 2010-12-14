9b694c8b9e0218a1c17e52d83188a609ad9b9234
Coda Hale <coda.hale@gmail.com>
2010-12-13 17:25:30 -0800

    Add the ability to compile Avro IDL files.

--- project/build/AvroSbt.scala
+++ project/build/AvroSbt.scala
@@ -1,8 +1,10 @@
 import sbt._
 
 class AvroSbt(info: ProjectInfo) extends PluginProject(info) with IdeaProject {
-  val avro = "org.apache.avro" % "avro" % "1.4.1" withSources() intransitive()
-  val jopt = "net.sf.jopt-simple" % "jopt-simple" % "3.2" withSources() intransitive()
+  val jboss = "JBoss" at "https://repository.jboss.org/nexus/content/groups/public/"
+
+  val avro = "org.apache.avro" % "avro" % "1.4.1" withSources()
+  val jopt = "net.sf.jopt-simple" % "jopt-simple" % "3.2" withSources()
   
 
   /**
--- src/main/scala/avro/AvroCompiler.scala
+++ src/main/scala/avro/AvroCompiler.scala
@@ -2,14 +2,32 @@ package avro
 
 import sbt._
 import org.apache.avro.specific.SpecificCompiler
+import org.apache.avro.tool.IdlTool
+import java.util.ArrayList
 
 trait AvroCompiler extends DefaultProject {
   override def compileOrder = CompileOrder.JavaThenScala
+
+  def avroIdls = "src" / "main" / "avro" ** "*.avdl"
   def avroSchemas = "src" / "main" / "avro" ** "*.avsc"
   def avroProtocols = "src" / "main" / "avro" ** "*.avpr"
   def avroOutputPath = "src" / "main" / "java"
+
+  private val idlTool = new IdlTool
   
   def generateAvroAction = task {
+    for (idl <- avroIdls.get) {
+      val tool = new IdlTool
+
+      log.info("Compiling IDL %s".format(idl))
+
+      val args = new ArrayList[String]
+      args.add(idl.toString)
+      args.add(idl.toString.replaceAll("""(.+?)(\.[^.]*$|$)""", "$1.avpr"))
+
+      tool.run(System.in, System.out, System.err, args)
+    }
+
     for (schema <- avroSchemas.get) {
       log.info("Compiling schema %s".format(schema))
       SpecificCompiler.compileSchema(schema.asFile, avroOutputPath.asFile)
